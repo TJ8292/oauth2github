7eb334ba0a51b6475ba428021919d1f2fea982d1
tchamba junias <tchambajunias@gmail.com>
2022-10-07 23:56:48 +0200

    Oauth details.

--- src/main/java/com/kuiteul/oauth2sso/controller/HomeController.java
+++ src/main/java/com/kuiteul/oauth2sso/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.kuiteul.oauth2sso.controller;
 
 
-import com.kuiteul.oauth2sso.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private Logger LOG = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/home")
-    public String home() {
+    public String home(OAuth2AuthenticationToken token) {
+        LOG.info(token.toString());
         return "home.html";
     }
 
