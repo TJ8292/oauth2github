fac16d625b7186f8c5f6be6b641bbafa301caca7
tchamba junias <tchambajunias@gmail.com>
2022-10-07 23:48:13 +0200

    test folder

---
+++ src/test/java/com/kuiteul/oauth2sso/Oauth2ssoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuiteul.oauth2sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ssoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
